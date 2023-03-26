a3b65a7103fb62b5ab8a7e7a026e4f8c4968ae72
Yi Ding <yi.s.ding@gmail.com>
2023-03-26 05:44:08 -0700

    image

---
+++ pages/api/image.ts
@@ -0,0 +1,33 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { Configuration, OpenAIApi } from "openai";
+
+const openai = new OpenAIApi(
+  new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  })
+);
+
+type Data = {
+  error?: string;
+  url?: string;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  if (req.method !== "POST") {
+    res.status(405).send({ error: "Only POST requests allowed" });
+    return;
+  }
+
+  const { word } = req.body;
+
+  const response = await openai.createImage({
+    prompt: `clip art of ${word}`,
+    n: 2,
+    size: "256x256",
+  });
+
+  res.status(200).json({ url: response.data.data[0].url });
+}

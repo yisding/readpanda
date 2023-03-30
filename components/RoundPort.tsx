c5d3f66463263cd7d089d97118c83a693cf4a45e
Yi Ding <yi.s.ding@gmail.com>
2023-03-29 19:58:21 -0700

    no mobile

--- components/RoundPort.tsx
+++ components/RoundPort.tsx
@@ -3,6 +3,10 @@ import { ReactNode } from "react";
 export default function RoundPort(props: { children?: ReactNode }) {
   return (
     <main className="w-full h-full flex justify-center items-center">
+      <div className="fixed w-screen h-screen z-10 font-bold text-4xl text-panda flex justify-center items-center bg-white flex-col lg:hidden">
+        <div>Busy building support for mobile</div>
+        <div>Sowwy</div>
+      </div>
       <div className="w-3/4 h-3/4 rounded-xl bg-gray-100 p-16">
         {props.children}
       </div>

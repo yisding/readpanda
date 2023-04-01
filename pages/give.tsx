e9d6bd3f7a4036c9f2556e414fe7d4f1aea8eef7
Yi Ding <yi.s.ding@gmail.com>
2023-03-31 17:31:41 -0700

    give

---
+++ pages/give.tsx
@@ -0,0 +1,12 @@
+export default function Give() {
+  return (
+    <iframe
+      src="https://docs.google.com/presentation/d/e/2PACX-1vTOuelUNYFgsJf6pkrCOIU53c5B12s1_-sXD1vgQgGVcNsFEIjE5KUvNGcZYl18Q-a-xWDy34fHP8Id/embed?start=false&loop=false&delayms=3000"
+      frameBorder="0"
+      width="1440"
+      height="839"
+      allowFullScreen={true}
+      className="h-full w-full"
+    ></iframe>
+  );
+}

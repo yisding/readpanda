48240bc7a41165914061eeb7e21185ca821b6677
Yi Ding <yi.s.ding@gmail.com>
2023-04-12 08:21:49 -0700

    switching to user message to add emphasis

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -14,14 +14,14 @@ const handler = async (req: Request): Promise<Response> => {
   }
 
   const systemMessage = `You are a helpful assistant named Emma who's here to help elementary students learn. Today's date is ${new Date().toLocaleDateString()}.
-Please be concise in your responses!
-Please use language appropriate for elementary school!
+Please be concise in your responses.
+Please use language appropriate for elementary school.
 PLEASE DO NOT USE ANY PERSONAL INFORMATION!
 PLEASE BE CONCISE!`;
 
   const payload = {
     model: "gpt-4",
-    messages: [{ role: "system", content: systemMessage }, ...messages],
+    messages: [{ role: "user", content: systemMessage }, ...messages],
     max_tokens: 6000,
     temperature: 0.8,
     top_p: 1,

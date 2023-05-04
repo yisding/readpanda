e1f2e43ca606153cfdffc52ab509ac3d93ab360a
Yi Ding <yi.s.ding@gmail.com>
2023-05-04 15:30:33 -0700

    more yoda

--- pages/chat.tsx
+++ pages/chat.tsx
@@ -9,7 +9,8 @@ import {
   MessageList,
 } from "@chatscope/chat-ui-kit-react";
 import Head from "next/head";
-import { useState } from "react";
+import { useRouter } from "next/router";
+import { useCallback, useEffect, useState } from "react";
 
 interface Message {
   role: "user" | "assistant";
@@ -17,66 +18,76 @@ interface Message {
 }
 
 export default function Chat() {
+  const router = useRouter();
+  const { word } = router.query;
+
   const [loading, setLoading] = useState(false);
   const [messages, setMessages] = useState<Message[]>([]);
 
-  const handleSend = async (
-    _innerHTML: string,
-    _textContent: string,
-    innerText: string
-  ) => {
-    setLoading(true);
+  const handleSend = useCallback(
+    async (_innerHTML: string, _textContent: string, innerText: string) => {
+      setLoading(true);
 
-    const message: Message = { role: "user", content: innerText };
-    const newMessages = [...messages, message];
-    setMessages(newMessages);
+      const message: Message = { role: "user", content: innerText };
+      const newMessages = [...messages, message];
+      setMessages(newMessages);
 
-    const response = await fetch("/api/yoda", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        messages: newMessages.slice(-8),
-      }),
-    });
-
-    if (!response.ok) {
-      throw new Error(response.statusText);
-    }
-
-    setMessages((prev) => [...prev, { role: "assistant", content: "" }]);
-
-    const data = response.body;
-    if (!data) {
-      return;
-    }
-    const reader = data.getReader();
-    const decoder = new TextDecoder();
-    let done = false;
-
-    while (!done) {
-      const { value, done: doneReading } = await reader.read();
-      done = doneReading;
-      const chunkValue = decoder.decode(value);
-      setMessages((prev) => {
-        let lastMessage = prev.at(-1);
-
-        if (!lastMessage) {
-          throw new Error("No message to append to.");
-        }
-
-        let newMessage = {
-          ...lastMessage,
-          content: lastMessage.content + chunkValue,
-        };
-
-        return [...prev.slice(0, -1), newMessage];
+      const response = await fetch("/api/yoda", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          messages: newMessages.slice(-8),
+        }),
       });
-    }
 
-    setLoading(false);
-  };
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+
+      setMessages((prev) => [...prev, { role: "assistant", content: "" }]);
+
+      const data = response.body;
+      if (!data) {
+        return;
+      }
+      const reader = data.getReader();
+      const decoder = new TextDecoder();
+      let done = false;
+
+      while (!done) {
+        const { value, done: doneReading } = await reader.read();
+        done = doneReading;
+        const chunkValue = decoder.decode(value);
+        setMessages((prev) => {
+          let lastMessage = prev.at(-1);
+
+          if (!lastMessage) {
+            throw new Error("No message to append to.");
+          }
+
+          let newMessage = {
+            ...lastMessage,
+            content: lastMessage.content + chunkValue,
+          };
+
+          return [...prev.slice(0, -1), newMessage];
+        });
+      }
+
+      setLoading(false);
+    },
+    [messages]
+  );
+
+  useEffect(() => {
+    if (word && typeof word === "string") {
+      if (messages.length === 0) {
+        handleSend("", "", word);
+      }
+    }
+  }, [handleSend, messages.length, word]);
 
   return (
     <>
